53d28ab10269353f971d06d36c5e55893fd4bd27
Thomas Gebert <thomas@gebert.sexy>
2018-04-06 00:27:44 -0400

    Finish implementing the display data stuff.
    All of this is untested for now...I imagine it'll be fun fully wiring this up

--- PostgresBackend/Postgres.fs
+++ PostgresBackend/Postgres.fs
@@ -184,13 +184,9 @@ module DisplayPreferenceHandlers =
         cmd.Parameters.AddWithValue("userid", userId) |> ignore
         let! result = cmd.ExecuteReaderAsync() |> Async.AwaitTask
         return SqlHelpers.ToDisplayPreference js result
-
-
-
-
     }
 
-    let saveDisplayPreferences (conn: NpgsqlConnection) (js: IJsonSerializer) (displayPreferences : DisplayPreferences) (userId: string) (client: string) (cancellationToken: CancellationToken) = async {
+    let saveDisplayPreferences (conn: NpgsqlConnection) (js: IJsonSerializer) (displayPreferences : DisplayPreferences) (userId: System.Guid) (client: string) (cancellationToken: CancellationToken) = async {
         cancellationToken.ThrowIfCancellationRequested()
         let query = """insert into userdisplaypreferences (id, userid, client, data) values (@id, @userid, @client, @data) on conflict update; """
         let datastring = js.SerializeToString displayPreferences
@@ -204,7 +200,23 @@ module DisplayPreferenceHandlers =
         return ()
         }
 
+    let saveAllDisplayPreferences conn js (displayPreferences: #seq<DisplayPreferences> ) (userid: System.Guid) (cancellationToken: CancellationToken) = 
+        cancellationToken.ThrowIfCancellationRequested()
+        displayPreferences
+        |> Seq.map (fun x ->
+            saveDisplayPreferences conn js x userid x.Client cancellationToken
+        )
+        |> Async.Parallel
+        |> Async.Ignore
 
+    let getDisplayPreferences conn js (dpid : string) (userid: string ) (client: string) =  async {
+        let query = """select data from userdisplaypreferences where userid = @userid and client = @client"""
+        let cmd = new NpgsqlCommand(query , conn)
+        cmd.Parameters.AddWithValue("userid", userid) |> ignore
+        cmd.Parameters.AddWithValue("client", client) |> ignore
+        let! result = cmd.ExecuteReaderAsync() |> Async.AwaitTask
+        return SqlHelpers.ToDisplayPreference js result
+        }
 
 type PostgresDisplayPreferencesRepository(connectionString, js : IJsonSerializer) =
     let conn = new NpgsqlConnection(connectionString)
@@ -213,13 +225,14 @@ type PostgresDisplayPreferencesRepository(connectionString, js : IJsonSerializer
         member this.Name with get () = "Postgres"
         member this.Dispose() = conn.Dispose()
         member this.SaveDisplayPreferences (displayPreferences, userid, client, cancellationToken) =
-            DisplayPreferenceHandlers.saveDisplayPreferences conn js displayPreferences userid client cancellationToken |> Async.RunSynchronously
+            DisplayPreferenceHandlers.saveDisplayPreferences conn js displayPreferences (System.Guid(userid)) client cancellationToken |> Async.RunSynchronously
 
         member this.GetAllDisplayPreferences (userId) =
             DisplayPreferenceHandlers.getAllDisplayPreferences conn js userId |> Async.RunSynchronously
 
             //failwith "placeholder for getalldisplaypreferences"
-        member this.SaveAllDisplayPreferences (displayPreferences, userid, cancellationToken) = failwith "placeholder for savealldisplaypreferences"
+        member this.SaveAllDisplayPreferences (displayPreferences, userid, cancellationToken) = //failwith "placeholder for savealldisplaypreferences"
+            DisplayPreferenceHandlers.saveAllDisplayPreferences conn js displayPreferences userid cancellationToken |> Async.RunSynchronously
         member this.GetDisplayPreferences (displayPreferenceId, userid, client) = failwith "placeholder for getdisplaypreferences"
 
 type PostgresUserLibrary(connectionString, js : IJsonSerializer) =
